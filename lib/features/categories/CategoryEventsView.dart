f957f0b8fa3bc02fbad6dbccadbd96faa418ea05
asmaa mousa <asmaamousa978@gmail.com>
2025-08-01 18:13:02 +0200

    refactor ui

--- lib/features/categories/CategoryEventsView.dart
+++ lib/features/categories/CategoryEventsView.dart
@@ -1,31 +1,54 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eventify_app/core/routes.dart';
+import 'package:eventify_app/features/add_event/edit%20event/edit_event_view.dart';
+import 'package:eventify_app/features/add_memory/view/add_memory.dart';
+import 'package:eventify_app/features/events/event_cubit/event_cubit.dart';
+import 'package:eventify_app/features/events/event_cubit/event_state.dart';
 import 'package:eventify_app/features/events/widgets/card_no_events.dart';
+import 'package:eventify_app/features/events/widgets/event_card.dart';
 import 'package:eventify_app/models.dart/event_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CategoryEventsView extends StatelessWidget {
+class CategoryEventsView extends StatefulWidget {
   const CategoryEventsView({super.key});
+
+  @override
+  State<CategoryEventsView> createState() => _CategoryEventsViewState();
+}
+
+class _CategoryEventsViewState extends State<CategoryEventsView> {
+  void initState() {
+    super.initState();
+    context.read<EventCubit>().fetchEvents();
+    context.read<EventCubit>().fetchInterestedEvents();
+  }
 
   @override
   Widget build(BuildContext context) {
     // final String category = ModalRoute.of(context)!.settings.arguments as String;
     final args = ModalRoute.of(context)!.settings.arguments;
- final bool isAll = args is List<String>;
-    final List<String> allCategories = isAll ? args as List<String> : [args as String];
+    final bool isAll = args is List<String>;
+    final List<String> allCategories =
+        isAll ? args as List<String> : [args as String];
     return Scaffold(
       appBar: AppBar(
         // title: Text(category),
         title: Text(isAll ? "All Events" : allCategories.first),
-
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('events')
-            // .where('category', isEqualTo: category)
-            .where('category', whereIn: allCategories.length > 10 ? allCategories.sublist(0, 10) : allCategories)
-
-            .snapshots(),
+        stream:
+            FirebaseFirestore.instance
+                .collection('events')
+                // .where('category', isEqualTo: category)
+                .where(
+                  'category',
+                  whereIn:
+                      allCategories.length > 10
+                          ? allCategories.sublist(0, 10)
+                          : allCategories,
+                )
+                .snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -35,12 +58,19 @@ class CategoryEventsView extends StatelessWidget {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
 
-          final List<EventModel> categoryEvents = snapshot.data!.docs
-              .map((doc) => EventModel.fromMap(doc.data() as Map<String, dynamic>))
-              .toList();
+          final List<EventModel> categoryEvents =
+              snapshot.data!.docs
+                  .map(
+                    (doc) =>
+                        EventModel.fromMap(doc.data() as Map<String, dynamic>),
+                  )
+                  .toList();
 
           if (categoryEvents.isEmpty) {
-            return CardNoEvents(text: '"No events found in this category."', title: 'Events Category');
+            return CardNoEvents(
+              text: '"No events found in this category."',
+              title: 'Events Category',
+            );
           }
 
           return ListView.builder(
@@ -48,70 +78,56 @@ class CategoryEventsView extends StatelessWidget {
             itemCount: categoryEvents.length,
             itemBuilder: (context, index) {
               final event = categoryEvents[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    AppRoutes.eventPreview, // عدلي لو اسمك مختلف
-                    arguments: event,
-                  );
-                },
-                child: Card(
-                  margin: const EdgeInsets.only(bottom: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  elevation: 4,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (event.bannerUrl != null)
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-                          child: Image.network(
-                            event.bannerUrl!,
-                            height: 180,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              event.title,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+              return BlocBuilder<EventCubit, EventState>(
+  builder: (context, state) {
+    final isInterested =
+        context.read<EventCubit>().isInterested(event.id);
+                  return GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  AppRoutes.eventPreview, // عدلي لو اسمك مختلف
+                                  arguments: event,
+                                );
+                              },
+              
+                              child: CardEvent(
+                                
+                                onToggleInterested: () {
+                                  context.read<EventCubit>().toggleInterestedEvent(event);
+                                  setState(() {}); //
+                                },
+                                isInterested:context
+                                              .read<EventCubit>()
+                                              .isInterested(event.id),
+                                event: event,
+                                onDelete: () {
+                                  context.read<EventCubit>().deleteEvent(event.id);
+                                },
+                                onEdit: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) {
+                                        return EditEventView(event: event);
+                                      },
+                                    ),
+                                  );
+                                },
+                                onJoin: () {
+                                  context.read<EventCubit>().joinEvent(event);
+                                },
+                                onAddMemory: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => AddMemory(event: event),
+                                    ),
+                                  );
+                                },
                               ),
-                            ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                const Icon(Icons.date_range, size: 16, color: Colors.grey),
-                                const SizedBox(width: 4),
-                                Text(event.date),
-                              ],
-                            ),
-                            const SizedBox(height: 4),
-                            Row(
-                              children: [
-                                const Icon(Icons.location_on, size: 16,
-                                    color: Colors.grey),
-                                const SizedBox(width: 4),
-                                Flexible(child: Text(event.location,
-                                  overflow: TextOverflow.ellipsis,)),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-           
+                            );
+                },
               );
             },
           );
@@ -120,4 +136,3 @@ class CategoryEventsView extends StatelessWidget {
     );
   }
 }
-
